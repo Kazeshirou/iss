e056a6daf56ac22188a1ebcf6dc39eb9f4175460
Natalie Zharova <zharntl@yandex.ru>
2021-05-03 12:31:44 +0300

    update: lab2: основной код вынесен из мейника

--- lab2/src/statistic.h
+++ lab2/src/statistic.h
@@ -2,7 +2,11 @@
 
 #include <cstddef>
 
+
 struct statistic_t {
-    size_t comparisons{0};
-    size_t transposition{0};
+    size_t empty_sequences{0};
+    size_t steps{0};
+    size_t operations{0};
+    size_t read{0};
+    size_t write{0};
 };
